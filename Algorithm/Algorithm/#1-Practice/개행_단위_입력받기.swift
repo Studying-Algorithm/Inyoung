c4d484bf2dac8b1d15671311042807da2e6c1641
6uohul <kiy010604@naver.com>
2023-08-31 19:04:16 +0900

    #1- 정수 여러 개 입력받기

--- Algorithm/Algorithm/#1-Practice/개행_단위_입력받기.swift
+++ Algorithm/Algorithm/#1-Practice/개행_단위_입력받기.swift
@@ -1,11 +1,16 @@
 //
-//  정수 여러 개 입력받기.swift
+//  여러가지 입력받기.swift
 //  Algorithm
 //
 //  Created by 김인영 on 2023/08/31.
 //
 
 import Foundation
+
+// 개행 단위 입력받기
+
+let input = readLine()!.split(separator: " ")
+print(input)
 
 // 정수 여러 개 입력받기
 
