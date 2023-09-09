27769016f1e9ba3da2b12680589ad7dc78b17b28
6uohul <kiy010604@naver.com>
2023-09-09 14:24:19 +0900

    #7- BOJ 2588 문제 세팅

--- Algorithm/Algorithm/BOJ/main.swift
+++ Algorithm/Algorithm/BOJ/main.swift
@@ -1,28 +1,9 @@
 //
-//  2601.swift
-//  약수 구하기
-//  Algorithm
+//  2588.swift
+//  Algorithm - 곱셈
 //
-//  Created by 김인영 on 2023/09/06.
+//  Created by 김인영 on 2023/09/09.
 //
 
 import Foundation
 
-let input: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
-var count = 0
-
-for i in 1...input[0] {
-    if input[0] % i == 0 {
-        count += 1
-        
-        if count == input[1] {
-            print(i)
-            count = -1
-            break
-        }
-    }
-}
-
-if count != -1 {
-    print(0)
-}
