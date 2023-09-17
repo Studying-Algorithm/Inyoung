93bb2ccfd1bc6f849654204266d7f6dbefef9bbb
6uohul <kiy010604@naver.com>
2023-09-17 19:46:04 +0900

    #9- 2163 문제세팅

---
+++ Algorithm/Algorithm/BOJ/1152.swift
@@ -0,0 +1,11 @@
+//
+//  1152.swift
+//  Algorithm
+//
+//  Created by 김인영 on 2023/09/10.
+//
+
+import Foundation
+
+let input = readLine()!.split(separator: " ")
+print(input.count)
--- Algorithm/Algorithm/BOJ/main.swift
+++ Algorithm/Algorithm/BOJ/main.swift
@@ -1,11 +1,8 @@
 //
-//  1152.swift
+//  main.swift
 //  Algorithm
 //
-//  Created by 김인영 on 2023/09/10.
+//  Created by 김인영 on 2023/09/17.
 //
 
 import Foundation
-
-let input = readLine()!.split(separator: " ")
-print(input.count)
