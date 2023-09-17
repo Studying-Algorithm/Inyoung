f27c06411555bcf434e4a15ff1f5ba6b7d08930a
6uohul <kiy010604@naver.com>
2023-09-17 20:06:30 +0900

    #9-2751- 문제세팅

--- Algorithm/Algorithm/BOJ/main.swift
+++ Algorithm/Algorithm/BOJ/main.swift
@@ -1,12 +1,8 @@
 //
-//  2163.swift
-//  초콜렛 자르기
+//  main.swift
+//  Algorithm
 //
 //  Created by 김인영 on 2023/09/17.
 //
 
 import Foundation
-
-let input: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
-
-print(input[1] - 1 + (input[0] - 1) * input[1])
